e4de7f20b071331c1d6b96e50f53535715ed95b9
Oliver Moeller <omoeller@uiowa.edu>
2020-04-19 19:27:29 -0500

    test5

--- app.py
+++ app.py
@@ -7,23 +7,25 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.urls import url_parse
 from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
 from functools import wraps
+
 import pymysql
-#import secrets
+import secrets
 import os
+
+dbuser = os.environ.get('DBUSER')
+dbpass = os.environ.get('DBPASS')
+dbhost = os.environ.get('DBHOST')
+dbname = os.environ.get('DBNAME')
 
 
 #conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
-
+conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(dbuser, dbpass, dbhost, dbname)
 # Open database connection
 #dbhost = secrets.dbhost
 #dbuser = secrets.dbuser
 #dbpass = secrets.dbpass
 #dbname = secrets.dbname
-conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(dbuser, dbpass, dbhost, dbname)
-dbuser = os.environ.get('DBUSER')
-dbpass = os.environ.get('DBPASS')
-dbhost = os.environ.get('DBHOST')
-dbname = os.environ.get('DBNAME')
+
 #db = pymysql.connect(dbhost, dbuser, dbpass, dbname)
 
 app = Flask(__name__)
